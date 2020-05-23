329fe9e89ff075e49928c6426a24a5d1579c34bd
Marwin34 <marcinwojsik@wp.pl>
2020-05-23 17:18:56 +0200

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-rm -rf node_modules
+# Clean up previous releases
+rm -rf *.tgz
+
+# Prepare sha256sum file of package content
+sha256sum manifest.json package.json *.js LICENSE README.md >SHA256SUMS
+
+# Install node dependancies
 npm install --production
-rm -f SHA256SUMS
-sha256sum manifest.json package.json *.js LICENSE README.md > SHA256SUMS
 rm -rf node_modules/.bin
-find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
+find node_modules \( -type f -o -type l \) -exec sha256sum {} \; >>SHA256SUMS
+
+# Create tar package
 TARFILE=$(npm pack)
 tar xzf ${TARFILE}
+rm ${TARFILE}
 cp -r node_modules ./package
 tar czf ${TARFILE} package
-rm -rf package
+
+# Clean working directiores and files
+rm -rf package node_modules
+rm -f SHA256SUMS
+
 echo "Created ${TARFILE}"
